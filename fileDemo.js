381c04a85d003ad07f9924a34758b21457667cb9
vinothkumar-m18 <vinothkumarm045@gmail.com>
2025-10-06 16:38:06 +0530

    commiting changes

--- fileDemo.js
+++
@@ -1,9 +0,0 @@
-import url from 'url'
-const urlString = 'https://www.google.com?q=hello+world';
-const urlObj = new URL(urlString);
-urlObj.searchParams.set('q','youtube');
-console.log(urlObj.searchParams);
-
-const params = new URLSearchParams(urlObj.search);
-params.set('q', 'twitter');
-console.log(params);
